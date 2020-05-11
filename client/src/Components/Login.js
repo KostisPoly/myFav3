e52d69698be65c87c702904a5af1e3ceebf31404
Kostis <polu_kostas@yahoo.gr>
2020-05-11 18:13:59 +0300

    validation login fixes

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -13,41 +13,54 @@ class Login extends Component {
             emailError: '',
             passwordError: '',
             validPassword: false,
-            validEmail: false,
-            errors: {}
+            validEmail: false
         };
 
         this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);      
+        this.onSubmit = this.onSubmit.bind(this);    
+        this.onFocus = this.onFocus.bind(this);      
     }
 
     onChange(e) {
         
         this.setState({ [e.target.name]: e.target.value});
         if (e.target.name === 'password') {
+            
             if(e.target.value.length < 6) {
                 this.setState({passwordError: 'Password must be at least 6 characters long!'});
-                this.setState({validPassword: true});
+                this.setState({validPassword: false});
             } else {
                 this.setState({passwordError: ''});
-                this.setState({validPassword: false});
+                this.setState({validPassword: true});
             }
         } else {
             let regEx = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
             if(regEx.test(e.target.value)) {
+
                 this.setState({emailError: ''});
-                this.setState({validEmail: false});
+                this.setState({validEmail: true});
             } else {
                 this.setState({emailError: 'Invalid Email'});
-                this.setState({validEmail: true});
+                this.setState({validEmail: false});
             }
         }
         
         
     }
 
+    onFocus(e){
+        if (!e.target.value) {
+            (e.target.name === 'email') ? this.setState({emailError: 'Empty'}) : this.setState({passwordError: 'Empty'});
+        }
+        console.log(this.state);
+    }
+
     onSubmit(e) {
         e.preventDefault();
+        if (this.state.validPassword && this.state.validEmail && this.state.emailError === ''  && this.state.passwordError === '' ) {
+            console.log('Valid!');
+        }
+        
     }
 
     render() {
@@ -63,12 +76,13 @@ class Login extends Component {
                         noValidate 
                         autoComplete="off" 
                         style={{width: '50vw', textAlign: 'center', margin: '0 auto', paddingTop: '10vh'}}>
-                        <FormControl error={this.state.validEmail} fullWidth={true} margin="dense">
+                        <FormControl error={this.state.emailError != ''} fullWidth={true} margin="dense">
                             <InputLabel htmlFor="email">Email address</InputLabel>
                             <Input id="email"
                                 name="email"
                                 value={this.state.email}
                                 onChange={this.onChange}
+                                onFocus={this.onFocus}
                                 aria-describedby="my-helper-text1" 
                                 endAdornment={
                                     <InputAdornment position="end">
@@ -78,12 +92,13 @@ class Login extends Component {
                             />
                             <FormHelperText error={true} id="my-helper-text1">{this.state.emailError}</FormHelperText>
                         </FormControl>
-                        <FormControl error={this.state.validPassword} fullWidth={true} margin="dense">
+                        <FormControl error={this.state.passwordError != ''} fullWidth={true} margin="dense">
                             <InputLabel htmlFor="password">Password</InputLabel>
                             <Input id="password"
                                 name="password"
                                 value={this.state.password}
                                 onChange={this.onChange}
+                                onFocus={this.onFocus}
                                 aria-describedby="my-helper-text2"
                                 endAdornment={
                                     <InputAdornment position="end">
